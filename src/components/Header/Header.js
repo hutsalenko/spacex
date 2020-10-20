b8842271779f690f33b32458d03378525f37ab52
Ivan <ivangutsalenko92@gmail.com>
2020-10-20 14:33:45 +0300

    upload

---
+++ src/components/Header/Header.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+import "./header.css";
+import logo from "../../logo.svg";
+
+const Header = (props) => (
+  <header className="header">
+    <Link to="/">
+      <img src={logo} alt="Logo Space X" className="logo" />
+    </Link>
+    <nav className="main-nav nav">
+      <ul className="list">
+        {props.rockets.map((item, index) => (
+          <li className="item" key={index}>
+            <Link
+              to="/rocket"
+              onClick={() => {
+                props.changeRocket(item);
+              }}
+              className="item-link"
+            >
+              {item}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+    <nav className="secondary-nav">
+      <ul className="list">
+        <li className="item">
+          <NavLink to="/" className="item-link" activeClassName="active" exact>
+            Home
+          </NavLink>
+        </li>
+        <li className="item">
+          <NavLink to="/calendar" className="item-link" activeClassName="active">
+            Calendar
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
